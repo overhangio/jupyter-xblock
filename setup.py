54712a16dc5408d88a492a771c3c7cc0374f33ec
Régis Behmo <regis@behmo.com>
2023-06-27 16:19:27 +0200

    v16.0.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with io.open(os.path.join(here, "README.md"), "rt", encoding="utf8") as f:
 
 setup(
     name="jupyter-xblock",
-    version="15.0.3",
+    version="16.0.0",
     description="Jupyter XBlock for Open edX",
     long_description=readme,
     long_description_content_type="text/markdown",
@@ -29,5 +29,15 @@ setup(
     install_requires=["xblock", "web-fragments"],
     entry_points={"xblock.v1": ["jupyter = jupyterxblock.xblock:JupyterXBlock"]},
     license="AGPLv3",
-    classifiers=["License :: OSI Approved :: GNU Affero General Public License v3"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
 )
